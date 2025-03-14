3d2601fd2022425f00a5085cc186608559603a67
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-14 14:41:51 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+#суммирую значение каждого 2-го вхождения тэга 
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/3/3.html')
+    tags = browser.find_elements(By.XPATH, "//div[@class='text']/p[2]")
+    sum=0
+    for p in tags:
+        res = p.text
+        if res is not None and res.isdigit():
+            sum+=int(res)
+    print(sum)
+    time.sleep(5)
