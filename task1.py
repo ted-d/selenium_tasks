5759c750d6f5644887ab116ea4fb348f2458f0e6
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-14 14:33:04 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,12 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+#поиск и клик по ссылке с получением текста 
 with webdriver.Chrome() as browser:
     browser.get('https://parsinger.ru/selenium/2/2.html')
     link = browser.find_element(By.PARTIAL_LINK_TEXT, '16243162441624')
     if link:
         el = link.click()
-        st = el.find_element(By.ID,'result')
+        st = browser.find_element(By.ID,'result')
         print(st.text)
     time.sleep(5)
