ca68904e2b673bb201362d8b814f144f54fc8be0
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-14 14:37:37 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,15 @@
-pass
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#извлекаю все абзацы забираю оттуда текст и если число суммирую
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/3/3.html')
+    tags = browser.find_elements(By.TAG_NAME, 'p')
+    sum=0
+    for p in tags:
+        res = p.text
+        if res is not None and res.isdigit():
+            sum+=int(res)
+    print(sum)
+    time.sleep(5)
