4aa157e28993751391fd4029581b8cf7217b518f
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-14 14:23:54 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,9 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/2/2.html')
+    input_form = browser.find_elementы(By.TAG_NAME, 'a').send_keys('Text')
+    time.sleep(5)
