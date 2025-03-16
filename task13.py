01ee11602a4944b934869504e43a63f012374388
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-16 16:20:36 +0500

    Update task13.py

--- task13.py
+++ task13.py
@@ -1,1 +1,13 @@
-
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#добавляем куку на сайт 
+url = 'https://parsinger.ru/selenium/6/6.3.3/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    browser.add_cookie({"name":"secretKey","value":"selenium123"})
+    browser.refresh()
+    
+    res = browser.find_element(By.ID,"password").text
+    print(res)
+    time.sleep(5)
