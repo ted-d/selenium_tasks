07fc680a1775c6476b82909f4c2fe07b8c03f653
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-16 14:38:00 +0500

    Update task8.py

--- task8.py
+++ task8.py
@@ -1,1 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+url = 'https://parsinger.ru/selenium/6/6.2/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    for i in range(2):
+        browser.find_element(By.TAG_NAME,"a").click()
+    browser.back()
+    browser.back()
+    browser.find_element(By.ID,"getPasswordBtn").click()
+    time.sleep(5)
