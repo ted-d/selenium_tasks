294e6f6c398bf2e2601dbb54e3e64464ef881911
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-16 15:43:11 +0500

    Update task010.py

--- task010.py
+++ task010.py
@@ -1,1 +1,10 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+url = 'https://parsinger.ru/selenium/6/6.3.1/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    cookie = browser.get_cookie('token_22')['value']
+    print(cookie)
+    time.sleep(5)
