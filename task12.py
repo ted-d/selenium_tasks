2deb8f1882216efaf6d367bd973436cc30080dd5
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-16 16:02:46 +0500

    Update task12.py

--- task12.py
+++ task12.py
@@ -1,1 +1,9 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+url = 'https://parsinger.ru/selenium/6/6.3.2/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    cookies = browser.delete_all_cookies()
+    time.sleep(15)
