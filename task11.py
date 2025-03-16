5105051f5f904585bd48ab47a2f5e504f76681a2
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-16 16:00:44 +0500

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,1 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+url = 'https://parsinger.ru/selenium/6/6.3/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    cookies = browser.get_cookies()
+    song = ''
+    for cookie in cookies:
+        song=cookie['name']
+    browser.find_element(By.ID,"phraseInput").send_keys(song)
+    browser.find_element(By.ID,"checkButton").click()
+    res = browser.find_element(By.ID,"result").text
+    print(res)
+    time.sleep(5)
