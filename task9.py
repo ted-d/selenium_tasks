c884d79376154ccf61312e992edf7155ad7210ac
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-16 14:48:38 +0500

    Update task9.py

--- task9.py
+++ task9.py
@@ -1,1 +1,9 @@
-
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#находим картинку по ID и делаем скриншот
+url = 'https://parsinger.ru/selenium/6/6.2.1/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    browser.find_element(By.ID,"this_pic").screenshot('foo.jpg')
+    time.sleep(5)
