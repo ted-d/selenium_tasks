4d4d6c0f5afcb01d775f81ebe13f859fdca64ef8
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-17 13:31:16 +0500

    Update task17.py

--- task17.py
+++ task17.py
@@ -1,1 +1,18 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#получаем куку с четным номером и сумируем ее значение
+url = 'https://parsinger.ru/methods/3/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    cookies = browser.get_cookies()
+    s=0
+    for cook in cookies:
+        ar=cook['name'].split("_")
+        if ar[2].isdigit() and int(ar[2])%2==0:
+            s+=int(cook['value'])
 
+    
+  
+    print(s)
+    time.sleep(5)
