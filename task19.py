f7182432ab749e798592fbb12237f4b656ab91e1
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-17 14:47:01 +0500

    Update task19.py

--- task19.py
+++ task19.py
@@ -1,1 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+url = 'https://parsinger.ru/selenium/5.5/2/1.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    blocks = browser.find_elements(By.CLASS_NAME,"text-field")
+    s=0
+    for block in blocks:
+        if not block.get_attribute('disabled'):
+            block.clear()
+
+    browser.find_element(By.ID,"checkButton").click()
+    alert = browser.switch_to.alert.text
+    
+  
+    print(alert)
+    time.sleep(5)
