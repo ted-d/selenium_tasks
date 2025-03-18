1eba99c1f443c7065d035be2413751a9b69647e5
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-18 16:23:17 +0500

    Update task7.1.py

--- task7.1.py
+++ task7.1.py
@@ -1,1 +1,17 @@
 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import json
+url = 'https://parsinger.ru/selenium/7/7.1/index.html'
+
+
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    height = browser.execute_script("return document.body.scrollHeight")
+    browser.execute_script(f"window.scrollBy(0,{height})")
+    time.sleep(2)
+    res = browser.find_element(By.ID,"secret-container").text
+    print(res)
+    time.sleep(5)
