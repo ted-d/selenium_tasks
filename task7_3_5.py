6b731348f981c61fe0c3f1cd4f26d1abe7d81d46
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-19 15:02:49 +0500

    Update task7_3_5.py

--- task7_3_5.py
+++ task7_3_5.py
@@ -1,1 +1,15 @@
-
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+url = 'https://parsinger.ru/selenium/7/7.3.2/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+      
+    element = browser.find_element(By.ID,"dblclick-area")
+    
+    action = ActionChains(browser)
+    action.double_click(element).perform()
+    password = browser.find_element(By.ID, "passwordContainer").text
+    print("Пароль:", password)
