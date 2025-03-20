84525028e1f88999d6573b1faee4581412264b1a
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-20 18:21:57 +0500

    Update task7_5_1.py

--- task7_5_1.py
+++ task7_5_1.py
@@ -1,1 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/scroll/2/index.html')
+    divs = browser.find_elements(By.CSS_SELECTOR, "[class='item']")
+    ar=[]
+    for div in divs:
+        div.find_element(By.TAG_NAME,"input").click()
+        span = div.find_element(By.TAG_NAME,"span").text
+        if span is not None and span.isdigit():
+            ar.append(span)
+        span = None
 
+    time.sleep(3)
+    ar = list(map(int,ar))
+
+    print(sum(ar))
