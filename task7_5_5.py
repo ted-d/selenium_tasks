19219ecd48daebd81349b57fa92b5031bb78e198
ted-d <81649171+ted-d@users.noreply.github.com>
2025-03-22 13:49:03 +0500

    Update task7_5_5.py

--- task7_5_5.py
+++ task7_5_5.py
@@ -1,1 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
+url = 'https://parsinger.ru/selenium/5.7/1/index.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    divs = browser.find_elements(By.XPATH,'//*[@id="floating-container"]/div')
+    for div in divs:
+        button = div.find_element(By.TAG_NAME,"button")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+        button.click()
+    alert_text = browser.switch_to.alert.text
+    print(alert_text)
+    
